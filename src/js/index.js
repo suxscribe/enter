8643210677b95402837be8fbfc4d64dc148ce7c8
Sergei Birukov <suxscribe@gmail.com>
2025-05-19 17:21:22 +0300

    Tweak splitting header transitions
    fix text transition shift

--- src/js/index.js
+++ src/js/index.js
@@ -11,8 +11,6 @@ import { burgerToggle } from './components/header';
 import { casesItemHover } from './components/cases-item-hover.js';
 import followCursor from './components/follow-cursor';
 
-// window.UIkit = UIkit; // fix not difined bug
-
 // some svg magic. SVG to shadow root
 function requireAll(r) {
   r.keys().forEach(r);
@@ -28,50 +26,55 @@ sal({
 Splitting();
 
 // DOCUMENT READY
-// COMMON SCRIPTS
-initSections();
-casesSlider();
-burgerToggle();
-// INITIALIZE SWIPERS
+document.addEventListener('DOMContentLoaded', () => {
+  initSections();
+  casesSlider();
+  burgerToggle();
 
-// form file input stuff
-document.querySelectorAll('.form').forEach((form) => {
-  form.reset();
+  // form file input stuff
+  document.querySelectorAll('.form').forEach((form) => {
+    form.reset();
+  });
+  validateForms();
+  formFileInput();
+
+  // HOVER
+  const sectionHeaderHover = new followCursor({
+    selector: '.header',
+    selectorInner: '.header__bg-ball img',
+  });
+
+  // show preview video when hovering over the case teaser
+  casesItemHover();
+
+  // clients balls out
+  let delay = 0;
+  const delayDelta = 50;
+  const clientsItems = document.querySelectorAll('.about__clients-item');
+  delay = delayDelta * clientsItems.length;
+
+  document.querySelectorAll('.about__clients-item').forEach((item) => {
+    item.dataset.salDelay = delay;
+    delay = delay - delayDelta;
+  });
+
+  // heading splitting by lines
+
+  const splitHeaderTitle = document.querySelector('.header__title');
+  const splitHeaderTitleResults = Splitting({
+    target: splitHeaderTitle,
+    by: 'lines',
+  });
+
+  let splitHeaderTimeline = new gsap.timeline();
+
+  splitHeaderTitleResults[0].lines.forEach((line) => {
+    splitHeaderTimeline.fromTo(
+      line,
+      { y: 20, opacity: 0 },
+      { duration: 0.5, y: 0, opacity: 1, ease: 'circ.out' },
+      '=-0.4'
+    );
+  });
+  splitHeaderTimeline.shiftChildren(0.5); // shift animation start to 1 second (pause at start)
 });
-validateForms();
-formFileInput();
-
-// HOVER
-const sectionHeaderHover = new followCursor({
-  selector: '.header',
-  selectorInner: '.header__bg-ball img',
-});
-
-// show preview video when hovering over the case teaser
-casesItemHover();
-
-// clients balls out
-let delay = 0;
-const delayDelta = 50;
-const clientsItems = document.querySelectorAll('.about__clients-item');
-delay = delayDelta * clientsItems.length;
-
-document.querySelectorAll('.about__clients-item').forEach((item) => {
-  item.dataset.salDelay = delay;
-  delay = delay - delayDelta;
-});
-
-// heading splitting by lines
-
-const splitHeaderTitle = document.querySelector('.header__title');
-const splitHeaderTitleResults = Splitting({
-  target: splitHeaderTitle,
-  by: 'lines',
-});
-
-let splitHeaderTimeline = new gsap.timeline();
-
-splitHeaderTitleResults[0].lines.forEach((line) => {
-  splitHeaderTimeline.from(line, { duration: 0.3, y: 40, opacity: 0 }, '=-0.2');
-});
-splitHeaderTimeline.shiftChildren(1); // shift animation start to 1 second (pause at start)
